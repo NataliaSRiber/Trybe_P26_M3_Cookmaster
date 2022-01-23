3553eb0a598f0bd786955cd8f87c32fe3892f78a
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-01-23 14:30:46 -0300

    requisito 7

--- src/services/recipes/validations.js
+++ src/services/recipes/validations.js
@@ -6,7 +6,6 @@ const createRecipe = Joi.object({
   preparation: Joi.string().min(4).max(200).required(),
 });
 
-// console.log(schema.validate({ name: "Eggnog", ingredients: "ovos, leite, creme de leite, acúcar", preparation: "coe os ovos, acrescente os ingredientes em uma panela e mexa" }).error);
 const updateRecipe = Joi.object({
   name: Joi.string().min(2).max(100),
   ingredients: Joi.string().min(2).max(100),
